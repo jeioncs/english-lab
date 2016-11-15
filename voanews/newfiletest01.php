44c31d986e7527c0168aef185da3f9169dce7cca
JAVIER ESCUDERO INFANTE <jescudero@us.es>
2016-11-15 11:29:01 +0100

    Subida desde bib (No sé si los directorios de publicacion varian /var/www frente a /var/www/html)

--- voanews/newfiletest01.php
+++ voanews/newfiletest01.php
@@ -1,46 +1,48 @@
 <?php
-// NO FUNCIONA
+// Technology Report
+
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-date_default_timezone_set('Europe/Madrid');
-$hoy=date('Y\-m\-j');
 
 $enlaces = array();
 $doc = new DOMDocument;
 $aux = new DOMDocument;
 
+date_default_timezone_set('Europe/Madrid');
+$hoy=date('Y\-m\-j');
 
 
-$page = iconv('UTF-8', 'UTF-8//IGNORE',file_get_contents("http://www.51voa.com/Education_Report_1.html"));
-$doc->loadHTML($page);
-$doc->formatOutput = true;
-$doc->normalizeDocument();
+for ($i = 1; $i <= 15; $i++) {
 
-$lista = $doc->getElementById('list');
-$linksyfechas = $lista->getElementsByTagName('li');
+	$htmlpage="http://www.51voa.com/Technology_Report_".$i.".html";
+	$page = iconv('UTF-8', 'UTF-8//IGNORE',file_get_contents($htmlpage));
+	$page= str_replace("</a>","",$page);
+	$page= str_replace("</li>","</a></li>",$page);
+	//print_r($page);
 
+	$doc->loadHTML($page);
+	
+	$lista = $doc->getElementById('list');
+	$links = $lista->getElementsByTagName('a');
 
-foreach($linksyfechas as $linkyfecha) {
-	//echo $linkyfecha->textContent."<br>";
-
-    $aux=$linkyfecha->ownerDocument->saveHTML($linkyfecha);
-    $links = $aux->getElementsByTagName('a');
-    echo $aux;
- /*   
-    foreach($links as $link) {
-	if ($link->getAttribute('target') === '_blank') {
-		$enlace=$link->ownerDocument->saveHTML($link);
-	//	if (preg_match ( '/[0-1][0-9]-[0-1]?[0-9]-[0-3]?[0-9]/' ,$enlace,$matches)){
-			$aux=$link->getAttribute('href');
-			$enlaceValido="\"".$matches[0]."\",\"http://www.51voa.com".$aux."\"<br>";
-			array_push($enlaces, $enlaceValido);
-	//	}
+	
+	
+	foreach($links as $link) {
+		if ($link->getAttribute('target') === '_blank') {
+			$enlace=$link->ownerDocument->saveHTML($link);
+			if (preg_match ( '/[0-1][0-9]-[0-1]?[0-9]-[0-3]?[0-9]/' ,$enlace,$matches)){
+				$aux=$link->getAttribute('href');
+				$enlaceValido="\"".$matches[0]."\",\"http://www.51voa.com".$aux."\"<br>";
+				array_push($enlaces, $enlaceValido);
+			}
+		}
+	
 	}
-    }*/
 
-}
+} // Fin del bucle for
 
 print_r($enlaces);
+
 
 ?>
